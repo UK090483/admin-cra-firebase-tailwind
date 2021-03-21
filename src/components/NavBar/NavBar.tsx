1802503958043727fd6ba0801a77450a3604a718
uk090483 <konradullrich@me.com>
2021-03-21 21:34:57 +0100

    b

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -4,7 +4,7 @@ export interface INavBarProps {}
 
 const NavBar: React.FC<INavBarProps> = (props) => {
   return (
-    <nav className="fixed top-0 left-0 w-full z-40 bg-gray-50 dark:bg-gray-900 md:flex-row md:flex-no-wrap md:justify-start flex items-center p-4 shadow-xl">
+    <nav className="fixed top-0 left-0 w-full z-40 bg-red-500 dark:bg-gray-900 md:flex-row md:flex-no-wrap md:justify-start flex items-center p-4 shadow-xl">
       <div className="w-full mx-autp items-center flex justify-between md:flex-no-wrap flex-wrap md:px-10 px-4">
         {props.children}
       </div>
